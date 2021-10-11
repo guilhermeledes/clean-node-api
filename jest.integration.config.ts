240776549900ffe0f8bcbc80401c49d098e05114
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-10-10 23:34:22 -0300

    chore: add jest configs and scripts

---
+++ jest.integration.config.ts
@@ -0,0 +1,3 @@
+import config from './jest.config'
+config.testMatch = ['**/?(*.)+(test).[t]s']
+export default config
---
+++ jest.unit.config.ts
@@ -0,0 +1,3 @@
+import config from './jest.config'
+config.testMatch = ['**/?(*.)+(spec).[t]s']
+export default config
