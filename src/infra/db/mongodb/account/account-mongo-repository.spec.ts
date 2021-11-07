db63d4185fa8428f7053a58f3db643a371093b56
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-07 18:53:44 -0300

    test: ensure AccountMongoRepository return an account on loadByToken success without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -68,4 +68,16 @@ describe('AccountMongoRepository', () => {
       expect(account.accessToken).toBe('any_token')
     })
   })
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken success without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({ ...makeFakeAccount(), accessToken: 'any_token' })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toBe('any_name')
+      expect(account?.email).toBe('any_email@mail.com')
+      expect(account?.password).toBe('any_password')
+    })
+  })
 })
