f6c3cd23c177c24aef840f4d0954bf9b1e629b03
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-09 13:24:52 -0300

    refactor: change model to use Omit

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,7 +6,7 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   readdirSync(path.join(__dirname, '..', 'routes')).map(async file => {
-    if (!file.includes('.test.')) {
+    if ((!file.includes('.test.')) && (!file.endsWith('.map'))) {
       (await import(path.join('..', 'routes', file))).default(router)
     }
   })
