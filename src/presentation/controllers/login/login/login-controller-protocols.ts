9663e55d0a9e3687b81b103bf1305409e9f9c23f
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-10 19:14:39 -0300

    refactor: organize project files

--- src/presentation/controllers/login/login/login-controller-protocols.ts
+++ src/presentation/controllers/login/login/login-controller-protocols.ts
@@ -1,4 +1,4 @@
 /* istanbul ignore file */
 export * from '@/presentation/protocols'
 export * from '@/presentation/protocols/validation'
-export * from '@/domain/usecases/authentication'
+export * from '@/domain/usecases/authentication/authentication'
