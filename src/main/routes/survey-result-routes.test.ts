cdc531ba9336451cac92e2cb2813232e8c3b0923
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-11 10:16:33 -0300

    test: ensure PUT /surveys/:surveyId/results return 403 on save survey result without accessToken

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Survey Routes', () => {
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({ answer: 'any_answer' })
+        .expect(403)
+    })
+  })
+})
