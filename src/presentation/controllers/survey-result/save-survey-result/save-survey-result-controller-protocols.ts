598c3c2692b02fed9474ec21facfee0e334f2119
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-11 09:39:58 -0300

    feat: ensure SaveSurveyResultController  call SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -1,4 +1,5 @@
 /* istanbul ignore file */
-export * from '@/presentation/protocols'
+export * from '@/domain/usecases/survey-result/save-survey-result'
 export * from '@/domain/usecases/survey/load-survey-by-id'
 export * from '@/domain/models/survey'
+export * from '@/presentation/protocols'
