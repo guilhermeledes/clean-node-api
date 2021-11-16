29be4c0989894a9e95dc6ab7b6d606f83554e896
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-16 13:57:32 -0300

    refactor: remove protocols from coverage

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     coverageDirectory: 'coverage',
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
-    coveragePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+    coveragePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/', 'protocols'],
     coverageReporters: ['text-summary', 'lcov'],
     preset: '@shelf/jest-mongodb',
     collectCoverageFrom: [
--- src/data/usecases/save-survey-result/db-save-survey-result-protocols.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result-protocols.ts
@@ -1,4 +1,3 @@
-/* istanbul ignore file */
 export * from '@/domain/models/survey-result'
 export * from '@/domain/usecases/survey-result/save-survey-result'
 export * from '@/data/protocols/db/survey-result/save-survey-result-repository'
