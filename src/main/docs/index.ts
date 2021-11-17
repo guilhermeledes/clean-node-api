bc500a0b71391e5eb14803a179497b1bf41e54cc
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-17 11:54:30 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,7 +1,24 @@
+import { loginPath } from './paths/login'
+import { account } from './schemas/account'
+import { loginParams } from './schemas/login-params'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'API',
     version: '1.0.0'
+  },
+  servers: [
+    { url: '/api' }
+  ],
+  tags: [
+    { name: 'Login' }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account,
+    loginParams
   }
 }
---
+++ src/main/docs/schemas/account.ts
@@ -0,0 +1,8 @@
+export const account = {
+  type: 'object',
+  properties: {
+    accessToken: {
+      type: 'string'
+    }
+  }
+}
