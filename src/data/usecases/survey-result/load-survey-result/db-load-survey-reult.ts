5c027188c74516ff5982256f98c449ba31a06995
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-18 09:16:17 -0300

    feat: ensure DbLoadSurveyResult call LoadSurveyResultRepository with correct value

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-reult.ts
@@ -0,0 +1,14 @@
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+
+export class DbLoadSurveyResult {
+  constructor (
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+  ) {}
+
+  async load (surveyId: string): Promise<SurveyResultModel> {
+    await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+
+    return null
+  }
+}
