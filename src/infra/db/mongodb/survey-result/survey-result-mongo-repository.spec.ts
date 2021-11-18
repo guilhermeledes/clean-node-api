e07480ef7eeb1eb9061ff6adf47218d063804f89
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-11-17 22:53:19 -0300

    fix: change save survey result to return all answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -56,6 +56,8 @@ describe('SurveyResultMongoRepository', () => {
       expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer)
       expect(surveyResult.answers[0].count).toBe(1)
       expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
 
     test('Should update a surveyResult on success if its not new', async () => {
@@ -74,6 +76,8 @@ describe('SurveyResultMongoRepository', () => {
       expect(surveyResult.answers[0].answer).toBe(newAnswer)
       expect(surveyResult.answers[0].count).toBe(1)
       expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
   })
 })
