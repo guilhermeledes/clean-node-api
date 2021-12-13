4a6b6d887806c6c426ab01d7b703898ffd78f43d
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-12-13 13:24:24 -0300

    feat: setup apollo server

---
+++ src/main/config/apollo-server.ts
@@ -0,0 +1,12 @@
+import resolvers from '@/main/graphql/resolvers'
+import typeDefs from '@/main/graphql/type-defs'
+import { ApolloServer } from 'apollo-server-express'
+import { Express } from 'express'
+
+export default (app: Express): void => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  })
+  server.applyMiddleware({ app })
+}
