5781ca5a4b22fad925b480faf0f0cde9f7795fbc
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-12-13 16:08:13 -0300

    test: ensure SignUp Mutation returns EmailInUseError if email is in use

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -91,5 +91,25 @@ describe('Login GraphQL', () => {
       expect(res.data.signUp.accessToken).toBeTruthy()
       expect(res.data.signUp.name).toBe('Ledes')
     })
+
+    test('Should return EmailInUseError if email is in use', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Ledes',
+        email: 'ledes@gmail.com',
+        password
+      })
+      const { mutate } = createTestClient({ apolloServer })
+      const res: any = await mutate(signUpMutation, {
+        variables: {
+          name: 'Ledes',
+          email: 'ledes@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('The received email is already in use')
+    })
   })
 })
